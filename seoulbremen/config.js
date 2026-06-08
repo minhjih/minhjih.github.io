2d369c6889743367cdda45de496fd6bb15de770f
Minji(Jaehyun Choi) <87082053+minhjih@users.noreply.github.com>
2026-06-08 21:13:21 +0900

    Update config.js

--- seoulbremen/config.js
+++ seoulbremen/config.js
@@ -17,7 +17,7 @@
 
 window.SEOUL_BREMEN_CONFIG = {
   // [A] 앱스 스크립트 웹앱 주소 (예: https://script.google.com/macros/s/AKfy.../exec )
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxhuUpWPiHWYrDWLCNvzEpIG3JnWD2EYedMQtFlF3JKAErD3OcJbRiGD222G3EWpD04uw/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwZKbr67XQypZieBQq8K9Ox4Vnfn0JNhiPxSsS-bALJorwqsr-fAuverT9vo58MYYjVVQ/exec",
 
   // [A] 웹에서 편집할 때 입력할 비밀번호 (앱스 스크립트의 EDIT_KEY와 똑같이!)
   EDIT_KEY: "bremen0101",
